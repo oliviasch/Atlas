25492c00dcc292aab13ac614671ef8dd2dc867d5
oliviasch <147622457+oliviasch@users.noreply.github.com>
2024-04-18 21:15:10 -0400

    Update main.cpp
    Arbitrary code for testing purposes - can be deleted later

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
+#include "Splay.h"
 #include <iostream>
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(){
+    Splay splay;
+    splay.insert("test", 6);
+    splay.levelOrder();
+    std::cout << std::endl;
+    splay.insert("test", 8);
+    splay.levelOrder();
+    std::cout << std::endl;
+    splay.insert("test", 4);
+    splay.insert("test", 7);
+    splay.insert("test", 23);
+    splay.insert("test", 16);
+    splay.insert("test", 42);
+    splay.insert("test", 89);
+    splay.levelOrder();
+    std::cout << std::endl;
+    splay.deleteTree();
     return 0;
 }
