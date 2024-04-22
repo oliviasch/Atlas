f33c5532ce3cc6656d41804f43a4328a8464bcd2
awu1130 <66101418+awu1130@users.noreply.github.com>
2024-04-22 03:56:41 -0400

    add searchData

--- Splay.h
+++ Splay.h
@@ -10,6 +10,7 @@ private:
     Node* splay(Node* helpRoot);
     Node* helperInsert(Node* helpRoot, std::string country, std::string data);
     static void helperSearch(Node* helpRoot, const std::string& country);
+    Node* helperSearchData(Node* helpRoot, const std::string& data);
     void helperDelete(Node* helpRoot);
     static void helperLevelOrder(Node* helpRoot);
     void rotateLeft(Node* helpRoot);
@@ -18,6 +19,7 @@ public:
     Splay() : root(nullptr) {}
     void insert(std::string country, std::string data);
     void search(const std::string& country);
+    void searchData(const std::string& data);
     void deleteTree();
     void levelOrder();
 };
